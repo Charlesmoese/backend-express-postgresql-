abb44314f530733f25522afed6c95920e934d878
Charles Moese <charlesmoesefilho@gmail.com>
2025-05-04 02:26:04 +0000

    add tratamento de erro

--- example_requests/GET_Secure_Endpoint.sh
+++ example_requests/GET_Secure_Endpoint.sh
@@ -1,3 +1,3 @@
 curl --request GET \
   --url 'http://localhost:3000/protected' \
-  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTY5ODBlOGNkNzcyNzlkN2ZhNGFhNSIsImlhdCI6MTc0NjMxMTE4NSwiZXhwIjoxNzQ2MzE0Nzg1fQ.vudx7KFMxekSPs1gI6TnvuXS_z6zRh3ALaU2FJc8rdo'
+  --header 'Authorization: Bearer yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzQ2MzI0MTIxLCJleHAiOjE3NDYzMjc3MjF9.7PkoYYXbkmx2Co0FYLWRxQcSh2etl6YkF5b7IwQMsT8'
